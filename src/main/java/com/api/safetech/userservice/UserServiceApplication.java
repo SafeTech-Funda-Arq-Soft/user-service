f68172dffbf68510275da8b597103596f8da5ff4
Debie <debiegarcia@hotmail.com>
2022-06-07 01:25:37 -0500

    Create User Service

---
+++ src/main/java/com/api/safetech/userservice/UserServiceApplication.java
@@ -0,0 +1,19 @@
+package com.api.safetech.userservice;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@OpenAPIDefinition
+@EnableJpaAuditing
+@EnableEurekaClient
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
